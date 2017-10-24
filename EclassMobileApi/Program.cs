c81ae6c06302c062ba4f77e15e8d7d1fc274c059
amoraitis <tasosthira7@gmail.com>
2017-10-24 17:04:39 +0300

    Add project files.

---
+++ EclassMobileApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EclassMobileApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
