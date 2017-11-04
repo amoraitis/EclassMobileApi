76cb124eab78a2cf2c458b0e4626b9a43f298a3c
amoraitis <tasosthira7@gmail.com>
2017-11-04 03:02:48 +0200

    Tools for courses added
    Figured out how to get "closed" courses directly

--- EclassMobileApi/Program.cs
+++ EclassMobileApi/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Flurl.Http;
 using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Collections.Specialized;
 
 namespace EclassMobileApi
 {
@@ -10,13 +14,15 @@ namespace EclassMobileApi
              _Login = "/modules/mobile/mlogin.php",
             _portfolio = "/modules/mobile/mportfolio.php",
             _UserCourses = "/modules/mobile/mcourses.php",
-            _OpenCourses = "/modules/mobile/mcourses.php";
+            _OpenCourses = "/modules/mobile/mcourses.php",
+            _tools = "/modules/mobile/mtools.php?course=";
         static string loginResult { get; set; }
         public static void Main(string[] args)
         {
-            string login ="", portfolio="", opencourses = "", userCourses = "", loginurl = _auebEclass +_Login;
+            string login = "", portfolio = "", opencourses = "", userCourses = "", loginurl = _auebEclass + _Login, rss = "";
             string username = "p3140138"; //Console.ReadLine();
             string password = "noveplef97"; //Console.ReadLine();
+            string tools = "", ansd = "";
             Task.Run(async () =>
             {
                 login = await (loginurl).PostUrlEncodedAsync(new { uname = username, pass = password }).ReceiveString();
@@ -35,13 +41,36 @@ namespace EclassMobileApi
                 opencourses = await (_auebEclass + _OpenCourses).GetStringAsync();
 
             }).GetAwaiter().GetResult();
+            Task.Run(async () =>
+            {
+                tools = await (_auebEclass + _tools + "INF261").PostUrlEncodedAsync(new { token = login }).ReceiveString();
+
+            }).GetAwaiter().GetResult();
+            Task.Run(async () => {ansd= await "https://eclass.aueb.gr/modules/announcements/?course=INF261".PostUrlEncodedAsync(new { token = login }).ReceiveString(); }).GetAwaiter().GetResult();
+
             Console.WriteLine("Login: "+loginurl + Environment.NewLine + loginResult);
             Console.WriteLine("Portfolio: " + Environment.NewLine + portfolio);
             Console.WriteLine("User Courses:" + Environment.NewLine + userCourses);
             Console.WriteLine("Open courses:" + Environment.NewLine + opencourses);
+            Console.WriteLine("Tools:" + Environment.NewLine + tools);
+            Console.WriteLine("aAnnadsafd:" + Environment.NewLine + ansd);
+            Console.WriteLine(rss);
             System.Console.ReadLine();
+            
         }
-        
+        static async Task<string> GetFeed()
+        {
+            String rss;
+            try
+            {
+                rss = await "https://eclass.aueb.gr/modules/announcements/rss.php?c=INF261".GetAsync().ReceiveString();
+                return rss;
+            }
+            catch(FlurlHttpException flurlExc)
+            {
+                return flurlExc.Call.HttpStatus.ToString();
+            }
+        }
 
     }
 }
