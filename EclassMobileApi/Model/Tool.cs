19bb348e8b8082a79d65876e609ffaad5548bce1
amoraitis <tasosthira7@gmail.com>
2017-11-06 23:42:20 +0200

    XML parsing for tools of a course
    Tool ViewModel Created

---
+++ EclassMobileApi/Model/Tool.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace EclassMobileApi.Model
+{
+    public class ToolViewModel
+    {
+        public List<Tool> Tools { get; set; }
+        public ToolViewModel()
+        {
+            Tools = new List<Tool>();
+        }
+        public void SetTools(string xml)
+        {
+            XDocument toolsDocument = XDocument.Load(GenerateStreamFromString(xml));
+            toolsDocument.Root.Elements("toolgroup").Elements("tool")
+                .Select(t => new Tool
+                {
+                    Name = t.Attribute("name").Value,
+                    Link = t.Attribute("redirect").Value,
+                    Type = t.Attribute("type").Value
+                }).Where(t=>t.IsNeeded()).ToList().ForEach(t=>Tools.Add(t));
+        }
+        private static Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+        override public string ToString()
+        {
+            string toolsName = "";
+            Tools.ForEach(t => { toolsName+=  t.Name + "\n"; });
+            return toolsName;
+        }
+    }
+    public class Tool
+    {
+        public string Name { get; set; }
+        //For this property "redirect" property from xml is used
+        public string Link { get; set; }
+        public string Type { get; set; }
+        public bool IsNeeded()
+        {
+            return Type.Equals("coursedescription") || Type.Equals("description") || Type.Equals("docs");
+        }
+    }
+}
--- EclassMobileApi/Program.cs
+++ EclassMobileApi/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Text;
 using System.Linq;
+using System.Xml;
 using System.Collections.Specialized;
 using HtmlAgilityPack;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ namespace EclassMobileApi
 {
     class Program
     {
+        
         static string _auebEclass = "https://eclass.aueb.gr",
              _Login = "/modules/mobile/mlogin.php",
             _portfolio = "/modules/mobile/mportfolio.php",
@@ -20,12 +22,13 @@ namespace EclassMobileApi
             _tools = "/modules/mobile/mtools.php?course=",
             _PortfolioPage = "/main/portfolio.php";
         static string LoginResult { get; set; }
+        static string login = "", portfolio = "", opencourses = "", userCourses = "", loginurl = _auebEclass + _Login, rss = "";
+        static string username = "p3140138"; //Console.ReadLine();
+        static string password = "noveplef97"; //Console.ReadLine();
+        static string tools = "", portfolioPage = "", UID = "", AnnouncementToken = "";
         public static void Main(string[] args)
         {
-            string login = "", portfolio = "", opencourses = "", userCourses = "", loginurl = _auebEclass + _Login, rss = "";
-            string username = "p3140138"; //Console.ReadLine();
-            string password = "noveplef97"; //Console.ReadLine();
-            string tools = "", portfolioPage = "", UID="", AnnouncementToken="";
+            
             Task.Run(async () =>
             {
                 login = await (loginurl).PostUrlEncodedAsync(new { uname = username, pass = password }).ReceiveString();
@@ -41,21 +44,24 @@ namespace EclassMobileApi
             }).GetAwaiter().GetResult();
             Task.Run(async () =>
             {
-                opencourses = await (_auebEclass + _OpenCourses).GetStringAsync();
+                opencourses = await (_auebEclass + _OpenCourses).PostUrlEncodedAsync(new { token = login }).ReceiveString();
 
             }).GetAwaiter().GetResult();
+            Model.ToolViewModel toolViewModel = new Model.ToolViewModel();
             Task.Run(async () =>
             {
                 tools = await (_auebEclass + _tools + "INF261").PostUrlEncodedAsync(new { token = login }).ReceiveString();
-
+                
+                toolViewModel.SetTools(tools);
             }).GetAwaiter().GetResult();
+            
             Task.Run(async () => {portfolioPage= await (_auebEclass+_PortfolioPage).PostUrlEncodedAsync(new { token = login }).ReceiveString(); }).GetAwaiter().GetResult();
             AnnouncementToken = GetToken("INF261", login);
             Console.WriteLine("Login: "+loginurl + Environment.NewLine + LoginResult);
             Console.WriteLine("Portfolio: " + Environment.NewLine + portfolio);
             Console.WriteLine("User Courses:" + Environment.NewLine + userCourses);
             Console.WriteLine("Open courses:" + Environment.NewLine + opencourses);
-            Console.WriteLine("Tools:" + Environment.NewLine + tools);
+            Console.WriteLine("Tools:" + Environment.NewLine+ tools + Environment.NewLine + toolViewModel.ToString());
             UID = GetUID(portfolioPage);
             //Console.WriteLine(portfolioPage);
             Console.WriteLine("Portfolio:" + Environment.NewLine + UID);
@@ -84,5 +90,12 @@ namespace EclassMobileApi
             return(myval.First().Attributes["href"].Value.Split('&').GetValue(2).ToString()).Split('=').Last().ToString();
         }
 
+        static Dictionary<string, string> GetDirectories(string courseUID, string loginToken)
+        {
+            Dictionary<string, string> listDictionary = null;
+
+            return listDictionary;
+        }
+        
     }
 }
