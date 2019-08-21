3d275bef1c640295853f1c2d41a9b9d4fffd0bea
Anastasios Moraitis <anas.moraitis@gmail.com>
2019-08-21 23:32:11 +0300

    Add a JSON converter for the EclassUser object

---
+++ EclassApi.Tests/EclassConverterTest.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using EclassApi.Converters;
+using EclassApi.Models;
+using Microsoft.DotNet.PlatformAbstractions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace EclassApi.Tests
+{
+    /// <summary>
+    /// The text fixture for <see cref="EclassConverterTest" />.
+    /// </summary>
+    [TestFixture]
+    public class EclassConverterTest
+    {
+        private readonly string _json;
+        public EclassConverterTest()
+        {
+            _json = File.ReadAllText(
+                Path.Combine(ApplicationEnvironment.ApplicationBasePath, ".content", "eclassuser.json"), 
+                Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Test for <see cref="EclassConverterTest" />.
+        /// </summary>
+        [Test]
+        public void Test()
+        {
+            EclassUser eclassUser = null;
+            Assert.DoesNotThrow(()=>eclassUser = JsonConvert.DeserializeObject<EclassUser>(_json, new ToolItemConverter()));
+            Assert.That((eclassUser.UserCourses.First().ToolViewModel.Tools.First(tool=>tool.Type == ToolType.announcements) as AnnouncementsTool)?.Content, Is.Not.Null);
+            var list = eclassUser.UserCourses.SelectMany(course => course.ToolViewModel.Tools.OfType<AnnouncementsTool>()).Select(tool=>tool.Content).ToList();
+            Assert.That(list, Has.Some.Not.Empty);
+            list.ForEach(content => Assert.That(content, Is.Not.Null));
+        }
+    }
+}
+
---
+++ EclassApi/Converters/ToolItemConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using EclassApi.Models;
+using Flurl.Util;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EclassApi.Converters
+{
+    public class ToolItemConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var item = JObject.Load(reader);
+            if (Enum.TryParse(item["Type"].Value<string>(), out ToolType toolType))
+            {
+                var name = item["Name"].Value<string>();
+                var link = item["Link"].Value<string>();
+                var tool = new ToolBase()
+                {
+                    Name =  name,
+                    Link = link,
+                    Type = toolType
+                };
+                switch (toolType)
+                {
+                    case ToolType.announcements:
+                        var content = item["Content"].Last.First;
+                        
+                        return new AnnouncementsTool(tool){Content = content.HasValues ? JsonConvert.DeserializeObject<List<Announcement>>(content.ToString()) : new List<Announcement>()};
+                    case ToolType.coursedescription:
+                        return new CourseDescriptionTool(tool){Content = item["Content"].Value<string>()};
+                    case ToolType.description:
+                        return new DescriptionTool(tool){Content = item["Content"].Value<string>() };
+                    case ToolType.docs:
+                        return new DocsTool(tool){RootDirectory = item["RootDirectory"].Value<string>(), RootDirectoryDownloadLink = item["RootDirectoryDownloadLink"].Value<string>()};
+                    default:
+                        return item.ToObject<ToolBase>();
+                }
+            }
+
+            return item.ToObject<ToolBase>();
+
+
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(ToolBase).IsAssignableFrom(objectType);
+        }
+    }
+}
